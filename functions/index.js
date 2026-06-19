e4b5fb0a499062af0b4a105d5acdb187c4a60747
Peace Tabernacle <peacetabernacle2016@gmail.com>
2026-06-19 18:34:28 +0200

    modified index.js

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const twilio = require("twilio");
 
 admin.initializeApp();
 
-/* SAFE INIT (won't crash Cloud Run) */
-let client;
-
-try {
-  client = twilio(
-    functions.config().twilio.sid,
-    functions.config().twilio.token
-  );
-} catch (e) {
-  console.log("Twilio not initialized yet");
-}
-
 exports.sendWhatsApp = functions.https.onCall(async (data) => {
-
-  const phone = data.phone;
-  const message = data.message;
-
-  if (!client) {
-    throw new functions.https.HttpsError(
-      "failed-precondition",
-      "Twilio not configured"
-    );
-  }
-
-  return client.messages.create({
-    from: "whatsapp:+14155238886",
-    to: `whatsapp:${phone}`,
-    body: message
-  });
-
+  return {
+    success: true,
+    message: "Function is working"
+  };
 });
