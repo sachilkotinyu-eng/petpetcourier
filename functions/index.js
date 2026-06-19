a77ce3b29dff10a7100ca258f35e4370c8f666ac
Peace Tabernacle <peacetabernacle2016@gmail.com>
2026-06-19 18:46:09 +0200

    modified index.js and ..//script.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,31 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const twilio = require("twilio");
 
 admin.initializeApp();
 
+/* Twilio client */
+const client = twilio(
+  functions.config().twilio.sid,
+  functions.config().twilio.token
+);
+
 exports.sendWhatsApp = functions.https.onCall(async (data) => {
-  return {
-    success: true,
-    message: "Function is working"
-  };
+
+  try {
+
+    const phone = data.phone;
+    const message = data.message;
+
+    return await client.messages.create({
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:${phone}`,
+      body: message
+    });
+
+  } catch (error) {
+    console.error("TWILIO ERROR:", error);
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+
 });
